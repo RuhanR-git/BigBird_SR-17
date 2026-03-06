5845e4ed3103d1066d8c7b71447cb528f0a50867
RuhanR <ruhan.raghu@gmail.com>
2026-03-05 22:35:32 -0700

    Hopefully set up correct JSONs to run path planner auto (for now)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,12 +1,8 @@
 package frc.robot;
 
-import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.SwerveSubsystem;
@@ -16,8 +12,6 @@ public class RobotContainer {
 
   private final SwerveSubsystem driveBase = new SwerveSubsystem();
   
-  private final SendableChooser<Command> autoChooser;
-
   private final CommandXboxController m_driverController = new CommandXboxController(
       OperatorConstants.kDriverControllerPort);
 
@@ -25,10 +19,6 @@ public class RobotContainer {
     registerNamedCommands();
 
     configureBindings();
-
-    autoChooser = AutoBuilder.buildAutoChooser();
-    
-    SmartDashboard.putData("Auto Chooser", autoChooser);
 
     SwerveInputStream driveInputStream = SwerveInputStream.of(
         driveBase.getSwerveDrive(),
@@ -44,16 +34,17 @@ public class RobotContainer {
 
   private void registerNamedCommands() {
     // These names must match the "Named Commands" you create in the PathPlanner App
-    NamedCommands.registerCommand("Test Auto", Commands.print("All hail MacBeth!"));
+    NamedCommands.registerCommand("Follow Test Path", driveBase.followPathCommand("Test Auto"));
   }
 
   private void configureBindings() {
     m_driverController.y().onTrue(driveBase.zeroGyroCommand());
     m_driverController.b().whileTrue(driveBase.lockPoseCommand());
+
+
   }
 
   public Command getAutonomousCommand() {
-    // 6. Return the command selected on the Dashboard
-    return autoChooser.getSelected();
+    return driveBase.buildAutoCommand("Test Auto");
   }
 }
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -7,6 +7,7 @@ import java.util.function.Supplier;
 import org.json.simple.parser.ParseException;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
@@ -18,6 +19,7 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import swervelib.SwerveDrive;
@@ -26,6 +28,7 @@ public class SwerveSubsystem extends SubsystemBase {
   
   private final SwerveDrive swerveDrive;
   private final Field2d m_field = new Field2d();
+  private boolean pathPlannerConfigured = false;
 
   public SwerveSubsystem() { 
     File directory = new File(Filesystem.getDeployDirectory(), "swerve");
@@ -43,27 +46,31 @@ public class SwerveSubsystem extends SubsystemBase {
   }
 
   public final void setupPathPlanner() {
-    try {
-        RobotConfig config = RobotConfig.fromGUISettings();
+    try 
+    {
+      RobotConfig config = RobotConfig.fromGUISettings();
+      AutoBuilder.configure(
+          this::getPose, 
+          swerveDrive::resetOdometry, 
+          swerveDrive::getRobotVelocity, 
+          (speeds, feedforwards) -> swerveDrive.drive(speeds), 
+          new PPHolonomicDriveController(
+              new PIDConstants(5.0, 0.0, 0.0), // Translation PID
+              new PIDConstants(5.0, 0.0, 0.0)  // Rotation PID
+          ),
+          config,
+          () -> {
+              var alliance = DriverStation.getAlliance();
+              return alliance.isPresent() && alliance.get() == DriverStation.Alliance.Red;
+          },
+          this);
 
-        AutoBuilder.configure(
-            this::getPose, 
-            swerveDrive::resetOdometry, 
-            swerveDrive::getRobotVelocity, 
-            (speeds, feedforwards) -> swerveDrive.drive(speeds), 
-            new PPHolonomicDriveController(
-                new PIDConstants(5.0, 0.0, 0.0), // Translation PID
-                new PIDConstants(5.0, 0.0, 0.0)  // Rotation PID
-            ),
-            config,
-            () -> {
-                var alliance = DriverStation.getAlliance();
-                return alliance.isPresent() && alliance.get() == DriverStation.Alliance.Red;
-            },
-            this
-        );
-    } catch (IOException | ParseException e) {
-        DriverStation.reportError("Failed to load PathPlanner config: " + e.getMessage(), e.getStackTrace());
+      pathPlannerConfigured = true;
+    } 
+    catch (IOException | ParseException e) 
+    {
+      pathPlannerConfigured = false;
+      DriverStation.reportError("Failed to configure PathPlanner: " + e.getMessage(), e.getStackTrace());
     }
 }
 
@@ -79,6 +86,48 @@ public class SwerveSubsystem extends SubsystemBase {
     return run(() -> swerveDrive.lockPose());
   }
 
+  public Command followPathCommand(String pathName) {
+    if (!pathPlannerConfigured) {
+      setupPathPlanner();
+    }
+
+    if (!pathPlannerConfigured) {
+      DriverStation.reportError(
+          "Cannot follow path '" + pathName + "' because PathPlanner AutoBuilder is not configured.",
+          false);
+      return Commands.none();
+    }
+
+    try {
+      return AutoBuilder.followPath(PathPlannerPath.fromPathFile(pathName));
+    } catch (Exception e) {
+      DriverStation.reportError("Failed to load path '" + pathName + "': " + e.getMessage(),
+          e.getStackTrace());
+      return Commands.none();
+    }
+  }
+
+  public Command buildAutoCommand(String autoName) {
+    if (!pathPlannerConfigured) {
+      setupPathPlanner();
+    }
+
+    if (!pathPlannerConfigured) {
+      DriverStation.reportError(
+          "Cannot build auto '" + autoName + "' because PathPlanner AutoBuilder is not configured.",
+          false);
+      return Commands.none();
+    }
+
+    try {
+      return AutoBuilder.buildAuto(autoName);
+    } catch (Exception e) {
+      DriverStation.reportError("Failed to load auto '" + autoName + "': " + e.getMessage(),
+          e.getStackTrace());
+      return Commands.none();
+    }
+  }
+
   public Pose2d getPose() {
     return swerveDrive.getPose();
   }
@@ -86,14 +135,6 @@ public class SwerveSubsystem extends SubsystemBase {
   public SwerveDrive getSwerveDrive() {
     return swerveDrive;
   }
-
-  // public void registerNamedCommands() {
-  //   // Example: Map the string "Intake" in the GUI to a command
-  //   // NamedCommands.registerCommand("Intake", intakeSubsystem.intakeCommand());
-    
-  //   // You can also use print commands for testing
-  //   NamedCommands.registerCommand("Marker1", Commands.print("Passed Marker 1"));
-  // }
 
   @Override
   public void periodic() {
