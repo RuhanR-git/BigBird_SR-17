805ebb2eff0710d481cd244c7c1d624dbad31166
thisismaddoxm <thisismaddoxm@gmail.com>
2026-03-09 17:57:28 -0600

    changed code to try and use a linear interpolating tree map to calculate a ShooterState class that holds our shooter speed and actuator position

--- src/main/java/frc/robot/commands/PrepareShot.java
+++ src/main/java/frc/robot/commands/PrepareShot.java
@@ -15,7 +15,7 @@ import frc.robot.subsystems.ShooterSubsystem;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Inches;
 
-public class PrepareShot extends Command{
+public class PrepareShot extends Command   {
 
     ShooterSubsystem shooter;
     HoodSubsystem hood;
@@ -32,18 +32,50 @@ public class PrepareShot extends Command{
         final Translation2d robotPosition = robotPoseSupplier.get().getTranslation();
         final Optional<Alliance> alliance = DriverStation.getAlliance();
         final Translation2d hubPosition;
-        if (alliance.isPresent()&& alliance.get() == Alliance.Blue){
-            hubPosition = new Translation2d(Inches.of(182.105), Inches.of(158.845));;
-        } else {
-            hubPosition = new Translation2d(Inches.of(469.115), Inches.of(158.845));
+        if (alliance.isPresent()   && alliance.get() == Alliance.Blu e ) {
+            hubPosition = new Translation2d(Inches.of(182.105), Inches.of(158.845));
         }
-        
         return Meters.of(robotPosition.getDistance(hubPosition));
+    } 
+ 
+
+    
+    
+    public double rpm;
+    public double pivotAngle;
+
+    public ShooterState(double rpm, double pivotAngle) {
+        this.rpm = rpm;
+        this.pivotAngle = pivotAngle;
     }
 
-    private double getLinearActuatorDistance(){
-        Distance hubDistance = getDistanceToHub();
+    @Override
+    public ShooterState interpolate(ShooterState endValue, double t) {
+        // 't' is a value from 0 to 1 representing how far we are between points
+        double lerpRPM = this.rpm + (endValue.rpm - this.rpm) * t;
+        double lerpPivot = this.pivotAngle + (endValue.pivotAngle - this.pivotAngle) * t;
         
+        return new ShooterState(lerpRPM, lerpPivot);
+    }
+
+
+
+    private ShooterState getShooterState(){
+        Distance hubDistance = getDistanceToHub();
+
+        // Distance (meters) -> ShooterState (RPM & Angle)
+        InterpolatingTreeMap<Double, ShooterState> shooterTable = new InterpolatingTreeMap<>();
+
+        // Add your calibration data
+        // Distance, new ShooterState(RPM, PivotPosition)
+        shooterTable.put(1.0, new ShooterState(1500, 10.0));
+        shooterTable.put(3.0, new ShooterState(2500, 25.0));
+
+        // To use it:
+        double distanceToTarget = 2.0; // We are halfway between our points
+        ShooterState currentSetpoints = shooterTable.get(distanceToTarget);
+
+        return currentSetpoints;
     }
 
 }
